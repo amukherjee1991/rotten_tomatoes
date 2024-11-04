4b42fd3ee8fd836a390cf1bcdadbc054611eb417
amukherjee1991 <arghyamukherjee1991@gmail.com>
2024-11-04 12:16:05 -0600

    added the functions to read files

--- classification.py
+++ classification.py
@@ -8,9 +8,18 @@ def unzip_folder(zip_path:str, extract_to:str):
         zip_ref.extractall(extract_to)
     print(f"Files extracted to: {extract_to}")
 
+def read_csv(file_path:str):
+    df = pd.read_csv(file_path)
+    return df
+
+
 
 def main():
     unzip_folder('datasets.zip', '.')
+    reviews = read_csv("datasets/rotten_tomatoes_critic_reviews_50k.csv")
+    movies = read_csv("datasets/rotten_tomatoes_movies.csv")
+    print(reviews)
+    print(movies)
 
 if __name__=="__main__":
     main()
